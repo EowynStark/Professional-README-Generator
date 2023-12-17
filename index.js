fe5e53367a4b4b696450122ecc4ff3fef8a5133d
EowynStark <andwellm89@gmail.com>
2023-12-16 19:27:08 -0600

    added functions to create README

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+// required packages and files
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// user input questionnaire 
+const questions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: 'What is your motivation for this project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What problem does this project solve for a user?',
+        },
+        {
+            type: 'checkbox',
+            name: 'installation',
+            message: 'Please select any necessary files to install for use:',
+            choices: ['Node', 'NPM Inquirer Version 8.2.4'],
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please describe how to best use this project:',
+        },
+        {
+            type: 'checkbox',
+            name: 'credits',
+            message: 'Please select any outside sources used in your project:',
+            choices: ['None', 'Tutor', 'Instructor', 'Peer Input'],
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Would you like to include a usage license?',
+            choices: ['No License','Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'The Unlicense'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub username:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address:'
+        },
+    ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile() {
+    inquirer.prompt(questions)
+        .then((answers) =>{
+            const markdownContent = generateMarkdown(answers);
+            if (markdownContent !== undefined) {
+            fs.writeFile('README.md', markdownContent);
+            console.log('README.md created successfully!');
+        } else {
+            console.error('Error: Markdown content is undefined.');
+        }
+    })
+    .catch((error)=> {
+        console.error('Error generating README:', error);
+    });
+};
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+writeToFile();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,84 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseLink = {
+    'No License': '',
+    'Mozilla Public License 2.0': 'https://opensource.org/licenses/MPL-2.0',
+    'Apache License 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'MIT License': 'https://opensource.org/licenses/MIT',
+    'The Unlicense': 'http://unlicense.org/',
+};
+const licenseBadge = {
+    'No License': '',
+    'Mozilla Public License 2.0': 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg',
+    'Apache License 2.0': 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
+    'MIT License': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    'The Unlicense': 'https://img.shields.io/badge/license-Unlicense-blue.svg',
+};
+// renders the license badge on the markdown file based on user choice
+// returns empty if none chosen
+function renderLicenseBadge(license) {
+    return licenseBadge[license] || '';
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// renders the license link portion of the markdown based on user choice
+// returns empty if none chosen 
+function renderLicenseLink(license) {
+    return licenseLink[license] ? `[${license}](${licenseLink[license]})` : '';
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// renders the license section of the markdown based on user choice
+// returns empty if none chosen 
+function renderLicenseSection(license) {
+    if (license === 'No License') {
+        return '';
+    }
+    return `## License
+    This project is licensed under the ${license} - see the [LICENSE](${licenseLink[license]})
+    `;
+};
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// renders credits portion of markdown file
+// returns empty if no additional credits needed
+function renderCreditSection(credits) {
+    if (credits.includes('None')) {
+        return '';
+    }
+    return `## Credits
+    With special thanks to the help of:
+    - ${credits.join('\n- ')}
+    `;
+};
 
+//generates markdown for README
+function generateMarkdown(answers) {
+    const licenseBadgeMarkdown = renderLicenseBadge(answers.license);
+    const licenseLinkMarkdown = renderLicenseLink(answers.license);
+    const licenseSectionMarkdown = renderLicenseSection(answers.license);
+    const creditsSectionMarkdown = renderCreditSection(answers.credits);
+  return `# ${answers.title}
+[!${licenseBadgeMarkdown}]
+${licenseLinkMarkdown}
+
+## Description
+${answers.motivation}
+
+${answers.description}
+
+## Installation
+Packages or Software needed for proper usage:
+- ${answers.installation.join('\n- ')}
+
+## Usage
+${answers.usage}
+
+
+${creditsSectionMarkdown}
+
+
+${licenseSectionMarkdown}
+
+## Contact
+For any questions on use or further additions contact me at:
+GitHub: [${answers.github}](https://github.com/${answers.github})
+Email: ${answers.email}
 `;
 }
 
